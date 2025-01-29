6add392e5ed3b25e771068d52b6a27f3cd3f31cf
ariefff666 <mariefpratama90@gmail.com>
2025-01-29 08:01:07 +0700

    Refactor: close registration form

--- components/organisms/PendaftaranForm.tsx
+++ components/organisms/PendaftaranForm.tsx
@@ -72,12 +72,11 @@ export default function PendaftaranForm() {
   });
 
   useEffect(() => {
-    // toast({
-    //   title: "Pendafataran telah ditutup",
-    //   description:
-    //     "Mohon maaf pendaftaran OPREC HMIF UNSRI 2024 telah ditutup,terimakasih telah mendaftar,see u next year!!",
-    // });
-    // router.push("/");
+    toast({
+      title: "Pendafataran telah ditutup",
+      description: "Mohon maaf pendaftaran OPREC HMIF UNSRI 2024 telah ditutup,terimakasih telah mendaftar,see u next year!!",
+    });
+    router.push("/");
   }, []);
 
   const router = useRouter();
